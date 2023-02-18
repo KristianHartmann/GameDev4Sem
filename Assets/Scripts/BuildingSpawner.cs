7de2c543d50b445e6f4f1e57ea1654b9a80ec7ef
FluffaTheDuck <cbp@posche.dk>
2023-02-18 23:28:35 +0100

    imported into main scene for handin

--- Assets/Scripts/BuildingSpawner.cs
+++ Assets/Scripts/BuildingSpawner.cs
@@ -6,22 +6,11 @@ public class BuildingSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
    public GameObject unitPrefab;
-    public float spawnInterval = 1.0f;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            StartCoroutine(SpawnUnit());
-        }
-    }
-
-    private IEnumerator SpawnUnit()
-    {
-        while (true)
-        {
-            Instantiate(unitPrefab, transform.position, transform.rotation);
-            yield return new WaitForSeconds(spawnInterval);
+            Instantiate(unitPrefab, transform.position, Quaternion.identity);
         }
     }
 }
