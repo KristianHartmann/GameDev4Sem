a8504dd27fe6619e4aa81ee680053563650ee620
Frederik Hess <cph-fp103@cphbusiness.dk>
2023-02-20 15:52:25 +0100

    about to add health bar

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -6,14 +6,18 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     public float speed = 5f; // speed of the enemy
-    public float attackRange = 2f; // range at which the enemy will attack the building
+    public float attackInterval = 1f; // interval between each attack
     public int attackDamage = 10; // damage the enemy will do when it attacks the building
 
     private Transform target; // target building (cube)
+    private BuildingHealth targetHealth; // reference to the target building's health script
+    private float attackTimer = 0f; // timer for the next attack
+    private float attackRange = 2; // range of the attack
 
     private void Start()
     {
         target = GameObject.FindGameObjectWithTag("BuildingSample").transform; // find the target building (cube) in the scene
+        targetHealth = target.GetComponent<BuildingHealth>(); // get the building's health script
     }
 
     private void Update()
@@ -23,8 +27,15 @@ public class EnemyMovement : MonoBehaviour
         if (distance <= attackRange)
         {
             // attack the building if the distance is within the attack range
-            BuildingHealth buildingHealth = target.GetComponent<BuildingHealth>(); // get the building's health script
-            buildingHealth.TakeDamage(attackDamage); // call the TakeDamage method to decrease the building's health
+            if (attackTimer >= attackInterval)
+            {
+                targetHealth.TakeDamage(attackDamage); // call the TakeDamage method to decrease the building's health
+                attackTimer = 0f; // reset the attack timer
+            }
+            else
+            {
+                attackTimer += Time.deltaTime; // increase the attack timer
+            }
         }
         else
         {
--- Assets/Scripts/BuildingHealth.cs
+++ Assets/Scripts/BuildingHealth.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class BuildingHealth : MonoBehaviour
 {
-    public int startingHealth = 100; // starting health of the building
+    public int startingHealth = 50; // starting health of the building
     private int currentHealth; // current health of the building
 
+    public Slider healthBar; // reference to the health bar slider
+    
     private void Start()
     {
+        slider.maxValue = startingHealth; // set the max value of the health bar slider
         currentHealth = startingHealth; // set the current health to the starting health
+        healthBar.maxValue = startingHealth; // set the max value of the health bar slider
+        healthBar.value = currentHealth; // set the value of the health bar slider to the current health
     }
 
     public void TakeDamage(int damage)
@@ -20,5 +25,12 @@ public class BuildingHealth : MonoBehaviour
         {
             Destroy(gameObject); // destroy the gameObject (building/cube) when its health is 0 or less
         }
+
+        healthBar.value = currentHealth; // update the value of the health bar slider to the current health
     }
 }
+
+
+
+
+
